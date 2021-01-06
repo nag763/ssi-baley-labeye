8d66845cb1b73aca81e37cb6a781a69421c78228
nag763 <loic.labeye@tutanota.de>
2021-01-06 19:44:51 +0100

    End, examples added

--- baley_labeye_ssi.py
+++ baley_labeye_ssi.py
@@ -8,6 +8,10 @@ import sys
 import re
 import argparse
 import os
+import matplotlib.pyplot as plt
+import string
+import random
+
 
 def getHash(alg: str, arg: str) -> str :
     """
@@ -48,6 +52,9 @@ def password_score(password : str) -> int:
 
 
 def write_each_result(results):
+    """
+    Write each result from each arg in a different file
+    """
     for current_result in results: 
         with open(f'reports{os.sep}{current_result["value"]}.md', 'w') as f:
             f.write(f"# Comparison for {current_result['value']}\n")
@@ -65,6 +72,9 @@ def write_each_result(results):
 
 
 def output_in_console(results):
+    """
+    Writes the output in the console
+    """
     for index, arg in enumerate(results):
         print(f"\n---\nArg {index}: {arg['value']}\t Score : {arg['score']} /5")
         for alg in arg['algs']:
@@ -74,28 +84,87 @@ def output_in_console(results):
             print(f"\t-Average time for {arg['trials']} : {alg['avgtime']}")
 
 
+def show_aggregate_results(results):
+    """
+    Show aggregate results per algorithms
+    """
+    # Sort by length of the key
+    results = sorted(results, key=lambda result: len(result['value']))
+    for index, alg in enumerate(results[0]['algs']):
+        vals = []
+        plt.title(f'Difference of time to compute for different sizes')
+        for result in results:
+            vals.append((result['length'], result['algs'][index]['avgtime']))
+        plt.plot([x[0] for x in vals], [y[1] for y in vals], label=f'{alg["name"]}')
+        plt.xlabel("Length of the key")
+        plt.ylabel("Time in s")
+        plt.legend()
+        plt.savefig(f'reports{os.sep}diffs.png')
+
+    plt.clf()
+    plt.style.use('ggplot')
+    x = [alg['name'] for alg in results[0]['algs']]
+    energy = [alg['digest_bs'] for alg in results[0]['algs']]
+    x_pos = [i for i, _ in enumerate(x)]
+    plt.bar(x_pos, energy, color='blue')
+    plt.xlabel("Algorithm")
+    plt.ylabel("Block size")
+    plt.title("Difference of block size per algorithms")
+    plt.xticks(x_pos, x)
+    plt.savefig(f'reports{os.sep}bs.png')
+    plt.clf()
+
+    plt.style.use('ggplot')
+    x = [alg['name'] for alg in results[0]['algs']]
+    energy = [alg['digest_size'] for alg in results[0]['algs']]
+    x_pos = [i for i, _ in enumerate(x)]
+    plt.bar(x_pos, energy, color='green')
+    plt.xlabel("Algorithm")
+    plt.ylabel("Digest size")
+    plt.title("Difference of digest size per algorithms")
+    plt.xticks(x_pos, x)
+    plt.savefig(f'reports{os.sep}ds.png')
+    plt.clf()
+
+    with open(f'reports{os.sep}compare_results.md', 'w') as f:
+        f.write(f"# Comparison of time differences for the algs : {', '.join([alg['name'] for alg in results[0]['algs']])}\n")
+        f.write(f"{'|'.join(['Length']+[alg['name'] for alg in results[0]['algs']])}\n")
+        f.write("|".join(["-" for i in range(len(results[0]['algs'])+1)]))
+        f.write("\n")
+        for result in results:
+            f.write("|".join([str(len(result['value']))]+[str(alg['avgtime']) for alg in result['algs']]))
+            f.write("\n")
+        f.write("![Difference between the algorithms](ds.png)\n")
+        f.write("![Difference between the algorithms](bs.png)\n")
+        f.write("![Difference between the algorithms](diffs.png)\n")
+
 def get_args():
     """
     Get args value from the cmd line
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('inputs', metavar='value', type=str, nargs='+', help='arguments to be hashed')
+    parser.add_argument('inputs', metavar='value', nargs='+', default=[], type=str, help='arguments to be hashed')
     parser.add_argument("--iters", "-i", metavar='i', default=10000, type=int, help="number of iterations")
+    parser.add_argument("--random", "-r", metavar='r', type=int, help="number of random elements")
     parser.add_argument("--algs", "-a", type=str, default=['sha256', 'md5'], nargs='+', help="algorithms to try")
     parser.add_argument("--output", "-o", nargs='?', type=bool, default=False, const=True, help="define if an output is required")
     parser.add_argument("--silent", "-s", nargs='?', type=bool, default=False, const=True, help="define if the result is printed in console")
+    parser.add_argument("--compare", "-c", nargs='?', type=bool, default=False, const=True, help="define if the result is printed in console")
     args = parser.parse_args()
+
     if args.inputs:
         argv = args.inputs
-    else:
+    if args.random:
+        argv = [''.join([random.choice(string.ascii_letters) for j in range(i)]) for i in range(args.random)] 
+    if len(argv) == 0:
         print("Provide arguments")
         sys.exit()
 
-    return argv, args.iters, args.algs, args.output, args.silent
+    return argv, args.iters, args.algs, args.output, args.silent, args.compare
 
 
 if __name__ == "__main__":
-    argv, iters, algs, write_in_file, silent = get_args()
+    argv, iters, algs, write_in_file, silent, compare = get_args()
     results = []
     for index, arg in enumerate(argv) :
         score = password_score(arg)
@@ -120,3 +189,5 @@ if __name__ == "__main__":
         output_in_console(results)
     if write_in_file:
         write_each_result(results)
+    if compare:
+        show_aggregate_results(results) 
--- reports/esigelec1A*.md
+++
@@ -1,9 +0,0 @@
-# Comparison for esigelec1A*
-
-Score of the argument : 5/5
-Number of trials for each hash : 10000
-Length of the argument : 11
-Algorithm|Hash|Digest size|Block size|Time
--|-|-|-|-
-sha1|e7b53e225c4c48f3a7e520ed074cd2960bd7c83b|20|64|6.43351899998379e-07
-sha256|8bf271b052a385e2e078976e624fe1d5279089806977c3559fe27a3fac66b39f|32|64|6.73698799982958e-07
