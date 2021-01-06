0e1cf82e69242d62e12a00245f2e9ba638529d00
nag763 <loic.labeye@tutanota.de>
2021-01-06 15:40:00 +0100

    Print the report in a separate dir

--- baley_labeye_ssi.py
+++ baley_labeye_ssi.py
@@ -7,6 +7,7 @@ import csv2md
 import sys
 import re
 import argparse
+import os
 
 def getHash(alg: str, arg: str) -> str :
     """
@@ -52,27 +53,21 @@ def get_args():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('inputs', metavar='value', type=str, nargs='+', help='arguments to be hashed')
-    parser.add_argument("--iters", "-i", metavar='i', type=int, help="number of iterations")
-    parser.add_argument("--algs", "-a", type=str, nargs='+', help="algorithms to try")
+    parser.add_argument("--iters", "-i", metavar='i', default=10000, type=int, help="number of iterations")
+    parser.add_argument("--algs", "-a", type=str, default=['sha256', 'md5'], nargs='+', help="algorithms to try")
+    parser.add_argument("--output", "-o", nargs='?', type=bool, default=False, const=True, help="define if an output is required")
     args = parser.parse_args()
     if args.inputs:
         argv = args.inputs
     else:
         print("Provide arguments")
         sys.exit()
-    if args.iters:
-        iters = args.iters
-    else:
-        iters = 1000000
-    if args.algs:
-        algs = args.algs
-    else:
-        algs = ["sha256", "md5"]
 
-    return argv, iters, algs
+    return argv, args.iters, args.algs, args.output
+
 
 if __name__ == "__main__":
-    argv, iters, algs = get_args()
+    argv, iters, algs, write_in_file = get_args()
     for index, arg in enumerate(argv) :
         score = password_score(arg)
         output = []
@@ -81,15 +76,17 @@ if __name__ == "__main__":
         output.append(f"Score of the argument : {score}/5")
         output.append(f"Number of trials for each hash : {iters}")
         output.append("|".join(["Algorithm","Hash","Digest size","Block size","Time"]))
-        output.append("|".join(["-","-","-","-"]))
+        output.append("|".join("-" for i in range(5)))
         print(f"\n---\nArg {index} : {arg}\t Score : {score} /5")
         for algorithm in algs:
             hash = getHash(algorithm, arg)
+            time_for_hash = time_hash_computation(algorithm, arg, int(iters))
             print(f"* {algorithm}: {hash.hexdigest()}")
             print(f"\t-Digest size : {hash.digest_size}")
             print(f"\t-Block size : {hash.block_size}")
-            print(f"\t-Average time for {iters} : {time_hash_computation(algorithm, arg, int(iters))}")
-            output.append(f"{algorithm}|{hash.hexdigest()}|{hash.digest_size}|{hash.block_size}|{time_hash_computation(algorithm, arg, int(iters))}")
-            with open(f'{arg}.md', 'w') as f:
-                for line in output:
-                    f.write(f'{line}\n')
+            print(f"\t-Average time for {iters} : {time_for_hash}")
+            output.append(f"{algorithm}|{hash.hexdigest()}|{hash.digest_size}|{hash.block_size}|{time_for_hash}")
+            if write_in_file:
+                with open(f'reports{os.path.sep}{arg}.md', 'w') as f:
+                    for line in output:
+                        f.write(f'{line}\n')
---
+++ reports/helloworld.md
@@ -0,0 +1,8 @@
+# Comparison for helloworld
+
+Score of the argument : 2/5
+Number of trials for each hash : 1
+Algorithm|Hash|Digest size|Block size|Time
+-|-|-|-|-
+sha256|936a185caaa266bb9cbe981e9e05cb78cd732b0b3280eb944412bb6f8f8f07af|32|64|3.983999704360031e-06
+sha1|6adfb183a4a2c94a2f92dab5ade762a47889a5a1|20|64|3.547999767761212e-06
