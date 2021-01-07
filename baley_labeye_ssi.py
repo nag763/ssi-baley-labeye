6dd68ce12b98ea0701ca9a6d4addf462f97f1ebb
nag763 <loic.labeye@tutanota.de>
2021-01-07 20:21:22 +0100

    Delete useless functions of the program

--- baley_labeye_ssi.py
+++ baley_labeye_ssi.py
@@ -26,56 +26,12 @@ def time_hash_computation(alg: str, arg: str, iters: int):
     return timeit.Timer(lambda:getHash(algorithm, arg).hexdigest()).timeit(iters)/iters
 
 
-def password_score(password : str) -> int:
-    """
-    Verify the strength of 'password'
-    Returns a dict indicating the wrong criteria
-    A password is considered strong if:
-        8 characters length or more
-        1 digit or more
-        1 symbol or more
-        1 uppercase letter or more
-        1 lowercase letter or more
-    """
-    # calculating the length
-    length_acceptable = len(password) > 8
-    # searching for digits
-    digit_acceptable = re.search(r"\d", password) is not None
-    # searching for uppercase
-    uppercase_acceptable = re.search(r"[A-Z]", password) is not None
-    # searching for lowercase
-    lowercase_acceptable = re.search(r"[a-z]", password) is not None
-    # searching for symbols
-    symbol_acceptable = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is not None
-    return length_acceptable + digit_acceptable + uppercase_acceptable + lowercase_acceptable + symbol_acceptable
-
-
-def write_each_result(results):
-    """
-    Write each result from each arg in a different file
-    """
-    for current_result in results: 
-        with open(f'reports{os.sep}{current_result["value"]}.md', 'w') as f:
-            f.write(f"# Comparison for {current_result['value']}\n")
-            f.write("\n")
-            f.write(f"Score of the argument : {current_result['score']}/5\n")
-            f.write(f"Number of trials for each hash : {current_result['trials']}\n")
-            f.write(f"Length of the argument : {current_result['length']}\n")
-            f.write("|".join(["Algorithm","Hash","Digest size","Block size","Time"]))
-            f.write("\n")
-            f.write("|".join(["-" for i in range(5)]))
-            f.write("\n")
-            for alg in current_result['algs']:
-                f.write(f"{alg['name']}|{alg['hash_digest']}|{alg['digest_size']}|{alg['digest_bs']}|{alg['avgtime']}")
-                f.write("\n")
-
-
 def output_in_console(results):
     """
     Writes the output in the console
     """
     for index, arg in enumerate(results):
-        print(f"\n---\nArg {index}: {arg['value']}\t Score : {arg['score']} /5")
+        print(f"\n---\nArg {index}: {arg['value']}")
         for alg in arg['algs']:
             print(f"* {alg['name']}: {alg['hash_digest']}")
             print(f"\t-Digest size : {alg['digest_size']}")
@@ -137,36 +93,29 @@ def get_args():
     Get args value from the cmd line
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('inputs', metavar='value', nargs='+', default=[], type=str, help='arguments to be hashed')
+    parser.add_argument('inputs', metavar='number_of_random_str', nargs='?', default=[], type=int, help='number of random strings to be hashed')
     parser.add_argument("--iters", "-i", metavar='i', default=10000, type=int, help="number of iterations")
-    parser.add_argument("--random", "-r", metavar='r', type=int, help="number of random elements")
     parser.add_argument("--algs", "-a", type=str, default=['sha256', 'md5'], nargs='+', help="algorithms to try")
-    parser.add_argument("--output", "-o", nargs='?', type=bool, default=False, const=True, help="define if an output is required")
     parser.add_argument("--silent", "-s", nargs='?', type=bool, default=False, const=True, help="define if the result is printed in console")
-    parser.add_argument("--compare", "-c", nargs='?', type=bool, default=False, const=True, help="define if a comparison between the output has to be saved")
     args = parser.parse_args()
 
     if args.inputs:
-        argv = args.inputs
-    if args.random:
-        argv = [''.join([random.choice(string.ascii_letters) for j in range(i)]) for i in range(args.random)] 
-    if len(argv) == 0:
+        argv = [''.join([random.choice(string.ascii_letters) for j in range(i)]) for i in range(args.inputs)] 
+    else:
         print("Provide arguments")
         sys.exit()
 
-    return argv, args.iters, args.algs, args.output, args.silent, args.compare
+    return argv, args.iters, args.algs, args.silent
 
 
 if __name__ == "__main__":
-    argv, iters, algs, write_in_file, silent, compare = get_args()
+    argv, iters, algs, silent = get_args()
     results = []
     for index, arg in enumerate(argv) :
-        score = password_score(arg)
         results.append({})
         current_arg = results[len(results)-1]
         current_arg['value'] = arg
         current_arg['length'] = len(arg)
-        current_arg['score'] = score
         current_arg['trials'] = iters
         current_arg['algs'] = []
         for algorithm in algs:
@@ -181,7 +130,5 @@ if __name__ == "__main__":
                 })
     if not silent:
         output_in_console(results)
-    if write_in_file:
-        write_each_result(results)
-    if compare:
-        show_aggregate_results(results) 
+
+    show_aggregate_results(results) 
--- example/reports/onedigest/example.md
+++
@@ -1,9 +0,0 @@
-# Comparison for example
-
-Score of the argument : 1/5
-Number of trials for each hash : 10000
-Length of the argument : 7
-Algorithm|Hash|Digest size|Block size|Time
--|-|-|-|-
-sha256|50d858e0985ecc7f60418aaf0cc5ab587f42c2570a884095a9e8ccacd0f6545c|32|64|6.499241000710754e-07
-md5|1a79a4d60de6718e8e5b326e338ae533|16|64|5.414752999058692e-07
